2134f0caa6a8685ef19e43b3a736275c4b45db3f
zirangu <ziran.gu0224@gmail.com>
2024-06-16 22:16:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-
 from flask import Flask, request, render_template
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model, Sequential
+from tensorflow.keras.layers import Embedding, SimpleRNN, LSTM, Dense, SpatialDropout1D
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
 app = Flask(__name__)
 
-# Load the trained model
-model = load_model('phishing_email_detector.keras')
-
-# Initialize the tokenizer
+# Load the tokenizer and fit on sample data
 tokenizer = Tokenizer(num_words=5000, lower=True, oov_token='<OOV>')
-
-# Example function to fit tokenizer on sample data
-# Replace `sample_data` with the actual data used during training
 sample_data = ["This is a sample email text."]  # Replace with your actual data
 tokenizer.fit_on_texts(sample_data)
 
@@ -25,11 +19,21 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     email_text = request.form['email_text']
+    model_type = request.form['model_type']
+    
     sequences = tokenizer.texts_to_sequences([email_text])
-    padded = pad_sequences(sequences, maxlen=100)  # Ensure the maxlen is the same as used during training
+    padded = pad_sequences(sequences, maxlen=100)
+    
+    if model_type == 'LSTM':
+        model = load_model('phishing_email_detector.keras')
+    elif model_type == 'RNN':
+        model = load_model('phishing_email_detector_2.keras')
+    else:
+        return render_template('index.html', prediction_text='Invalid model type selected.')
+    
     prediction = model.predict(padded)
     is_phishing = (prediction > 0.5).astype(int)
-
+    
     return render_template('index.html', prediction_text='Phishing' if is_phishing[0][0] == 1 else 'Not Phishing')
 
 if __name__ == "__main__":
